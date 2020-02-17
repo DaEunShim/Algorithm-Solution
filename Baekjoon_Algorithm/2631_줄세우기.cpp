bdda30f409387754d0226136cc377a21bae85343
DaEunShim <10ekdms01@naver.com>
2020-02-17 20:48:21 +0900

    [2631] 줄세우기

---
+++ Baekjoon_Algorithm/2631_줄세우기.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+#include<vector>
+using namespace std;
+
+int main(void) {
+	int n, max=0;
+	cin >> n;
+	vector<int> v(n);
+	vector<int> dx(n);
+	for (int i=0; i < n; i++) {
+		cin >> v[i];
+	}
+	for (int i = 0; i < n; i++) {
+		dx[i] = 1;
+		for (int j = 0; j < i; j++) {
+			if (v[j] < v[i] && dx[i] < dx[j] + 1) {
+				dx[i]++;
+			}
+		}
+		if (max < dx[i]) max = dx[i];
+	}
+	cout << n - max << endl;
+	return 0;
+}
