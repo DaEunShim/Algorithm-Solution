684375b95a2cfc290ed6e15dd0749a451defa250
DaEunShim <10ekdms01@naver.com>
2020-04-05 23:05:39 +0900

    8.2 와일드카드

---
+++ Algos_AlgorithmStudy/8.2_와일드카드.cpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+string arr[51];
+int main() {
+	int c;
+	cin >> c;
+	while (c--) {
+		string s;
+		int n,cnt=0;
+		cin >> s;
+		cin >> n;
+
+
+		while (n--) {
+
+
+		}
+		for (int i = 0; i < cnt; i++) {
+			cout << arr[i] << endl;
+		}
+	}
+	return 0;
+}
