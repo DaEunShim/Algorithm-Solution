250b456a4a1f7a61d6a27d4c561a1ba4039f9d0f
DaEunShim <10ekdms01@naver.com>
2020-04-15 20:25:12 +0900

    전화번호 목록

---
+++ Programmers/전화번호 목록.cpp
@@ -0,0 +1,15 @@
+#include <string>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+bool solution(vector<string> phone_book) {
+    int len = phone_book.size();
+    sort(phone_book.begin(), phone_book.end());
+    for(int i = 0; i < len-1; i++){
+        if(phone_book[i].find(phone_book[i+1],0)==0) return false;
+        if(phone_book[i+1].find(phone_book[i],0)==0) return false;
+    }    
+    return true;
+}
