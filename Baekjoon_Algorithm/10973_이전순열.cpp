57b588fd6a28c9b855c7d957e897efe06066ef58
DaEunShim <10ekdms01@naver.com>
2020-05-01 02:41:27 +0900

    [10973] 이전순열

---
+++ Baekjoon_Algorithm/10973_이전순열.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+using namespace std;
+
+int arr[10000];
+int main() {
+	int n, first = 0;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+		if (i != 0 && arr[i] < arr[i - 1]) first = 1;
+	}
+	if (first == 0) {
+		cout << "-1";
+		return 0;
+	}
+	int i = n - 1;
+	for (; i >= 0; i--)
+		if (arr[i - 1] > arr[i]) break;
+	int j = n - 1;
+	for (; j >= i; j--)
+		if (arr[j] < arr[i - 1]) break;
+	swap(arr[i - 1], arr[j]);
+	for (int k = 0; k < i; k++)
+		cout << arr[k] << " ";
+	for (int k = n - 1; k >= i; k--)
+		cout << arr[k] << " ";
+	return 0;
+}
