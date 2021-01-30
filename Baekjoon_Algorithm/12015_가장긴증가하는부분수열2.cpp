59112d6fe610be25878298522cfc8405c2aabaa5
DaEunShim <10ekdms01@naver.com>
2021-01-30 19:58:46 +0900

    [12015] 가장 긴 증가하는 부분 수열2

---
+++ Baekjoon_Algorithm/12015_가장긴증가하는부분수열2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    vector<int> temp;
+    for(int i = 0; i < n; i++)
+        cin >> arr[i];
+
+    temp.push_back(arr[0]);
+    for(int i = 1; i < n; i++){
+        if(temp.back() < arr[i]) temp.push_back(arr[i]);
+        else{
+            int lb = lower_bound(temp.begin(), temp.end(), arr[i]) - temp.begin();
+            temp[lb] = arr[i];
+        }
+    }
+
+    cout<< temp.size() <<endl;
+    
+    return 0;
+}
