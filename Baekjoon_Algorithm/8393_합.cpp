455451f06972442df7000647ea7a1e9c834e3436
DaEunShim <10ekdms01@naver.com>
2021-02-03 23:24:03 +0900

    [8393] 합

---
+++ Baekjoon_Algorithm/8393_합.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int n, sum = 0;
+    cin >> n;
+    for(int i = 1; i <= n; i++)
+        sum += i;
+    cout<< sum <<endl;
+    return 0;
+}
