ea44323b2f5b248d713fe3788596c836ff2230f5
DaEunShim <10ekdms01@naver.com>
2021-02-15 23:51:39 +0900

    [2470] 두용액

---
+++ Baekjoon_Algorithm/2470_두용액.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cstdlib>
+
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int n;
+    cin >> n;
+    vector<int> arr(n);
+    for(int i = 0; i < n; i++){
+        cin >> arr[i];
+    }
+    sort(arr.begin(), arr.end());
+    long long sum, minimum = -9999999999;
+    int L = 0, R = n - 1, left = 0, right = n - 1;
+    while (L < R){
+         long long sum = arr[left] + arr[right];
+         if (abs(sum) < minimum) {
+         	minimum = abs(sum);
+            left = L;
+           	right = R;
+         }
+         if (sum < 0)  L++;
+         else R--;
+}
+
+    cout<< arr[left] <<" "<<arr[right]<<endl;
+    return 0;
+}
