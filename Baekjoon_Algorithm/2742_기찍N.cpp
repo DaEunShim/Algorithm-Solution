829301b5aa26f8a929961f3fb53caf1d4abddf2d
DaEunShim <10ekdms01@naver.com>
2021-02-21 23:39:43 +0900

    [2742] 기찍N

---
+++ Baekjoon_Algorithm/2742_기찍N.cpp
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    ios::sync_with_stdio(false); cin.tie(NULL);
+    int n;
+    cin >> n;
+    do{
+        cout << n << "\n";
+    }while(--n);
+
+    return 0;
+}
